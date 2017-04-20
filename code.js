15db5e36502100e0d95584e7f683b89e34325d43
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-04-20 22:17:30 +0200

    better error tracking and a cool spinner

--- code.js
+++ code.js
@@ -6,8 +6,15 @@ var key = "https://api.themoviedb.org/3/movie/550?api_key="
 
 function collectQuery() {
   var field = document.getElementById('searchField').value;
-  sendData(field);
-  document.getElementById('search').reset();
+  if (field == '') {
+    spinner.error();
+  } else {
+    clearAll();
+    spinner.show();
+    sendData(field);
+    document.getElementById('search').reset();
+  }
+
 }
 
 function sendData(searchQuery) {
@@ -16,18 +23,25 @@ function sendData(searchQuery) {
   let query = searchQuery;
 
   axios.get(httpReq + query).then(function(response) {
-    console.log(response);
-    let title = response.data.Title;
-    let year = response.data.Year;
-    let description = response.data.Plot;
-    let poster = response.data.Poster;
-    let link = response.data.imdbID;
-    let imdbRating = response.data.Ratings[0].Value;
-    let rtRating = response.data.Ratings[1].Value;
-    let mcRating = response.data.Ratings[2].Value;
-    let rating = Number(imdbRating.slice(0, 3));
-    renderMovie(title, year, description, poster, link);
-    renderVerdict(imdbRating, rtRating, mcRating, rating);
+    ratingArr = response.data.Ratings;
+    if (ratingArr.length !== 3) {
+      spinner.error();
+    } else {
+      let title = response.data.Title;
+      let year = response.data.Year;
+      let description = response.data.Plot;
+      let poster = response.data.Poster;
+      let link = response.data.imdbID;
+      let imdbRating = response.data.Ratings[0].Value;
+      let rtRating = response.data.Ratings[1].Value;
+
+      let mcRating = response.data.Ratings[2].Value;
+      let rating = Number(imdbRating.slice(0, 3));
+      spinner.hide();
+      renderMovie(title, year, description, poster, link);
+      renderVerdict(imdbRating, rtRating, mcRating, rating);
+    }
+
   }).catch(function(error) {
     console.log(error);
   });
@@ -76,4 +90,35 @@ function renderMovie(title, year, description, poster, link, rating, quality) {
 
   let outputArea = document.getElementById('output');
   outputArea.innerHTML = '<img src="' + poster + '">' + '<h1>' + title + ' ' + '[' + year + ']' + '</h1>' + '<hr>' + '<p>' + description + '</p>' + '<a target="_blank" href="http://www.imdb.com/title/' + link + '"><button>More info</button></a>';
+}
+
+var spinner = {
+  show: function() {
+    document.getElementById('center').innerHTML =
+  `<div class="sk-cube-grid">
+  <div class="sk-cube sk-cube1"></div>
+  <div class="sk-cube sk-cube2"></div>
+  <div class="sk-cube sk-cube3"></div>
+  <div class="sk-cube sk-cube4"></div>
+  <div class="sk-cube sk-cube5"></div>
+  <div class="sk-cube sk-cube6"></div>
+  <div class="sk-cube sk-cube7"></div>
+  <div class="sk-cube sk-cube8"></div>
+  <div class="sk-cube sk-cube9"></div>
+</div>`
+  },
+  hide: function() {
+    document.getElementById('center').innerHTML = ''
+  },
+  error() {
+    clearAll();
+    document.getElementById('center').innerHTML =
+    `<p>Please enter a valid movie or check your spelling</p>`
+  }
+}
+
+function clearAll() {
+  document.getElementById('verdict').innerHTML = ''
+  document.getElementById('output').innerHTML = ''
+
 }
