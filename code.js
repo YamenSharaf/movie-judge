be9e9b3cd41a073f9b3cdf8f8c834c67d2b86368
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-04-20 20:14:31 +0200

    beautify

--- code.js
+++ code.js
@@ -4,7 +4,6 @@ document.getElementById('search').addEventListener('submit', function(e) {
 
 var key = "https://api.themoviedb.org/3/movie/550?api_key="
 
-
 function collectQuery() {
   var field = document.getElementById('searchField').value;
   sendData(field);
@@ -16,31 +15,28 @@ function sendData(searchQuery) {
   let httpReq = "https://www.omdbapi.com/?t="
   let query = searchQuery;
 
-  axios.get(httpReq + query)
-    .then(function(response) {
-      let title = response.data.Title;
-      let year = response.data.Year;
-      let description = response.data.Plot;
-      let poster = response.data.Poster;
-      let link = response.data.imdbID;
-      let imdbRating = response.data.Ratings[0].Value;
-      let rtRating = response.data.Ratings[1].Value;
-      let mcRating = response.data.Ratings[2].Value;
-      let rating = Number(imdbRating.slice(0, 3));
-      renderMovie(title, year, description, poster, link);
-      renderVerdict(imdbRating, rtRating, mcRating, rating);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  axios.get(httpReq + query).then(function(response) {
+    let title = response.data.Title;
+    let year = response.data.Year;
+    let description = response.data.Plot;
+    let poster = response.data.Poster;
+    let link = response.data.imdbID;
+    let imdbRating = response.data.Ratings[0].Value;
+    let rtRating = response.data.Ratings[1].Value;
+    let mcRating = response.data.Ratings[2].Value;
+    let rating = Number(imdbRating.slice(0, 3));
+    renderMovie(title, year, description, poster, link);
+    renderVerdict(imdbRating, rtRating, mcRating, rating);
+  }).catch(function(error) {
+    console.log(error);
+  });
 
 }
 
 function renderVerdict(imdb, rt, mc, total) {
   let quality = determineQuality(total);
   let verdictOutput = document.getElementById('verdict')
-  verdictOutput.innerHTML =
-  `
+  verdictOutput.innerHTML = `
     <div class = "imdb">
     <p class="site">IMDb Rating</p>
     <p class="score">${imdb}</p>
@@ -63,37 +59,20 @@ function renderVerdict(imdb, rt, mc, total) {
 
 function determineQuality(rating) {
   if (rating > 8.0) {
-    return (
-      '<h3 class="spectacular"> Spectacular movie. A must watch </h3>'
-    )
+    return ('<h3 class="spectacular"> Spectacular movie. A must watch </h3>')
   } else if (rating > 7.0) {
-    return (
-      '<h3 class="great"> Great watch </h3>'
-    )
+    return ('<h3 class="great"> Great watch </h3>')
   } else if (rating > 6.0) {
-    return (
-      '<h3 class="okay"> Not the best of movies </h3>'
-    )
+    return ('<h3 class="okay"> Not the best of movies </h3>')
   } else if (rating > 5.0) {
-    return (
-      '<h3 class="bad"> Just terrible. Don\'t waste your time </h3>'
-    )
+    return ('<h3 class="bad"> Just terrible. Don\'t waste your time </h3>')
   } else {
-    return (
-      '<h3 class="error"> Doesn\'t even exist.' + rating + ' Check your spelling </h3>'
-
-    )
+    return ('<h3 class="error"> Doesn\'t even exist.' + rating + ' Check your spelling </h3>')
   }
 }
 
 function renderMovie(title, year, description, poster, link, rating, quality) {
 
   let outputArea = document.getElementById('output');
-  outputArea.innerHTML =
-    '<img src="' + poster + '">' +
-
-    '<h1>' + title + ' ' + '(' + year + ')' + '</h1>' +
-    '<hr>' +
-    '<p>' + description + '</p>' +
-    '<a target="_blank" href="http://www.imdb.com/title/' + link + '"><button>More info</button></a>';
+  outputArea.innerHTML = '<img src="' + poster + '">' + '<h1>' + title + ' ' + '(' + year + ')' + '</h1>' + '<hr>' + '<p>' + description + '</p>' + '<a target="_blank" href="http://www.imdb.com/title/' + link + '"><button>More info</button></a>';
 }
